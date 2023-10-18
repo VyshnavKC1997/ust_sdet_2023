5047c6891e2fd903ee90998bfbf603b056708803
VyshnavKC1997 <Vyshnav.KunnumalChalil@ust.com>
2023-10-18 17:36:55 +0530

    Assignment added

---
+++ Assignment-1-oct-18/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Assignment_1_oct_18;
+
+Console.WriteLine("enter the name");
+string? name=Console.ReadLine();
+Console.WriteLine("Enter the 3 marks");
+int markOne=Convert.ToInt32(Console.ReadLine());
+int markTwo = Convert.ToInt32(Console.ReadLine());
+int markThree = Convert.ToInt32(Console.ReadLine());
+Student student=new Student(name, markOne, markTwo, markThree);
+Console.WriteLine("\nName :" + name +"\nTotal mark :"+student.TotalMark()+ "\nAverage:" + student.Average()+"\nGrade:"+student.TotalGrade());
---
+++ Assignment-1-oct-18/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1_oct_18
+{
+    internal class Student
+    {
+        string? name;
+        int markOne,markTwo,markThree;
+
+        public Student(string? name, int markOne, int markTwo, int markThree)
+        {
+            this.name = name;
+            this.markOne = markOne;
+            this.markTwo = markTwo;
+            this.markThree = markThree;
+        }
+        public double Average()
+        {
+            return (markOne + markTwo + markThree)/3;
+        }
+        public double TotalMark()
+        {
+            return markOne + markTwo + markThree;
+        }
+        public char TotalGrade()
+        {
+            double average=Average();
+            if (average > 90)
+                return 'A';
+            else if (average > 80)
+                return 'B';
+            else if (average > 70)
+                return 'C';
+            else if (average > 60)
+                return 'D';
+            else
+                return 'E';
+        }
+    }
+}
