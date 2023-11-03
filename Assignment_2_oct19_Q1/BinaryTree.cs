37a3d81f742f5e49fa9952fe79297b2af0db7978
VyshnavKC1997 <Vyshnav.KunnumalChalil@ust.com>
2023-11-03 14:50:42 +0530

    added binary tree

---
+++ Assignment_2_oct19_Q1/BinaryTree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2_oct19_Q1
+{
+    internal class BinaryTree
+    {
+    }
+}
